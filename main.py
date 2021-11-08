559e44df2de593a7b8521727390eb98311cdfc89
yuantse <yuantsechang@gmail.com>
2021-11-08 11:12:10 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import tensorflow
+import keras
+import numpy
+import sklearn
+import pandas
+
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(f"tensorflow version={tensorflow.__version__}")
+    print(f"keras version={keras.__version__}")
+    print(f"numpy version={numpy.__version__}")
+    print(f"pandas version={pandas.__version__}")
+    print(f"sklearn version={sklearn.__version__}")
