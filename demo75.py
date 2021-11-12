c73893790846f7efef8542929f474a2d4725134f
yuantse <yuantsechang@gmail.com>
2021-11-12 15:43:30 +0800

    day5 demo75

--- demo75.py
+++ demo75.py
@@ -5,13 +5,17 @@ imageSourceArray = tf.constant([1, 1, 1, 0, 0, 0] * 6, tf.float32)
 print(imageSourceArray)
 # 排列原始圖片為6*6
 images = tf.reshape(imageSourceArray, [1, 6, 6, 1])
+# 直的轉成橫的
+images = tf.transpose(images, perm=[0, 2, 1, 3])
 # 顯示原始圖片矩陣
 print(images[0, :, :, 0])
-#filterSourceArray = tf.constant([1, 0, -1] * 3, tf.float32)
+# filterSourceArray = tf.constant([1, 0, -1] * 3, tf.float32)
 # 定義filter(patch kernel)
 filterSourceArray = tf.constant([1, 0, -1] * 3, tf.float32)
 # 排列filter為3*3
 filter = tf.reshape(filterSourceArray, [3, 3, 1, 1])
+# 直的轉成橫的
+filter = tf.transpose(filter, perm=[1, 0, 2, 3])
 # 顯示filter矩陣
 print(filter[:, :, 0, 0])
 # 進行convolution
